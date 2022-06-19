474f32191526b660aed027126fd84948d70b63c7
uisproject <uisprojectxyz@gmail.com>
2022-06-19 07:14:39 +0700

    added how to create custom middleware in redux/toolkit and set true for devTools

--- src/features/store.js
+++ src/features/store.js
@@ -1,10 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
+
 import counterReducer from "./counter";
 import getPostReducer from "./getPost";
 import userReducer from "./reducers/userReducers";
 
+// creating custom middleware like expressjs
+// this will be super usefull if you want to intercept the api
+export const customMiddleware = (store) => (next) => (action) => {
+  console.log("Middleware 1");
+  console.log("Middleware triggered:", action);
+
+  // modify the payload
+  if (action.type === "counterSlice/counting") {
+    action.payload = action.payload * 10;
+  }
+
+  next(action);
+};
+
+export const customMiddleware2 = (store) => (next) => (action) => {
+  console.log("Middleware 2");
+  next(action);
+};
+
+// middleware from redux
+const logger = createLogger();
+
+const middleware = [logger, customMiddleware, customMiddleware2]; // this will be executed in order for the custom middleware, idk why the logger is executed last as per logging
+
 const store = configureStore({
-  // when you add a new reducer make sure to register the object property into utils/selectorName.js too
+  // applying middleware
+  middleware: getDefaultMiddleware().concat(middleware),
+  devTools: true,
   reducer: {
     counter: counterReducer,
     getPost: getPostReducer,
